3d331ef530e558329b0f8cf88e4bc1918ac2a239
santiago <santiagodeandrade4@gmail.com>
2024-12-11 11:58:27 -0400

    Modificacion para conexion de prueba a mongoDB

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Configuración de la URI de conexión
+const dbUser = "pysanti"; // Reemplaza con tu nombre de usuario de MongoDB
+const dbPassword = "16102005"; // Reemplaza con tu contraseña de MongoDB
+const dbName = "proyecto_topicos_db"; // Nombre de tu base de datos
+const uri = `mongodb://localhost:27017/${dbName}`; // Cambia localhost si es necesario
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Conectado a MongoDB");
+    })
+    .catch((error) => {
+        console.error("Error de conexión a MongoDB:", error);
+    });
+app.use(express.json());
+
+// Rutas de ejemplo
 app.get("/", (req, res) => {
-    res.send("¡Hola, mundo desde Express.js!");
+    res.send("¡Hola, mundo desde Express.js y MongoDB!");
 });
 
+// Inicia el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
