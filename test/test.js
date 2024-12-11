245324fb741eb64c5ea57042fc88f5f5ae1648b4
santiago <santiagodeandrade4@gmail.com>
2024-12-11 15:53:59 -0400

    Modificacion de archivo test.js

--- test/test.js
+++ test/test.js
@@ -1,15 +1,14 @@
-const request   = require('supertest');
-const expect    = require('chai').expect;
-const app       = require('../app');
+import request from "supertest"
+import app from "../app.js"
 
 describe('GET /', function() {
     it('should return status 200 and a message', function(done) {
     request(app)
         .get('/')
-        .expect(200)
+        .expect.expect(200)
         .end(function(err, res) {
         if (err) return done(err);
-        expect(res.body.message).to.equal('Hello, world!');
+        expect.expect(res.body.message).to.equal('Hello, world!');
         done();
         });
     });
