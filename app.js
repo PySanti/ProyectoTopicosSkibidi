c779b2d5a6739a9495089a7237d678a6f34a50e6
santiago <santiagodeandrade4@gmail.com>
2024-12-11 12:07:59 -0400

    Prueba de uso de mongoDB y mongoose

--- app.js
+++ app.js
@@ -10,24 +10,34 @@ const dbPassword = "16102005"; // Reemplaza con tu contraseña de MongoDB
 const dbName = "proyecto_topicos_db"; // Nombre de tu base de datos
 const uri = `mongodb://localhost:27017/${dbName}`; // Cambia localhost si es necesario
 mongoose
-    .connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(uri, {})
     .then(() => {
         console.log("Conectado a MongoDB");
     })
     .catch((error) => {
         console.error("Error de conexión a MongoDB:", error);
     });
-app.use(express.json());
 
-// Rutas de ejemplo
+const usuarioSchema = new mongoose.Schema({
+    nombre: String,
+    edad: Number,
+    email: String,
+});
+const Usuario = mongoose.model("Usuario", usuarioSchema);
+const nuevoUsuario = new Usuario({
+    nombre: "Maria",
+    edad: 22,
+    email: "maria@example.com",
+});
+nuevoUsuario
+    .save()
+    .then(() => console.log("Usuario guardado"))
+    .catch((err) => console.error("Error al guardar usuario:", err));
+
+app.use(express.json());
 app.get("/", (req, res) => {
     res.send("¡Hola, mundo desde Express.js y MongoDB!");
 });
-
-// Inicia el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
