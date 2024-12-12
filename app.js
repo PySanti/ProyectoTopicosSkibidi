1b3108fb8de93067c83704aefde1438a78b850c6
santiago <santiagodeandrade4@gmail.com>
2024-12-11 22:13:58 -0400

    Creacion incial de req_1

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-import express  from "express";
+import express from "express";
 import { connect_to_mongo } from "./db/connect_to_mongo.js";
 import { save_sample_user } from "./db/save_sample_user.js";
+import { req_1 } from "./controllers/req_1.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
-connect_to_mongo();
-save_sample_user();
-app.get("/", (req, res) => {
-    res.send("¡Hola, mundo desde Express.js y MongoDB!");
-});
+app.get("/", req_1); // 1er requisito mandatorio
+
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
---
+++ controllers/req_1.js
@@ -0,0 +1,4 @@
+export function req_1(req, res){
+    res.send("Saludos desde req 1")
+    console.log(req.params)
+}
