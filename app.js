82d5120e232617ede44c7cf68d6903931bfe9838
AlejandroFlow2000 <alejanch2004@gmail.com>
2025-01-07 13:46:30 -0400

    Agregue el tercer requerimiento: Put

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { connect_to_mongo } from "./db/connect_to_mongo.js";
 import { req_1 } from "./controllers/req_1.js"; 
 import { saveJoke } from "./controllers/req_2.js";
+import{validarObjectId, actualizarJoke } from "./controllers/req_3.js"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ connect_to_mongo();
 app.get("/", req_1);
 app.get("/joke", req_1); 
 app.post("/joke", saveJoke);
+app.put("/joke/:id", validarObjectId,actualizarJoke);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
