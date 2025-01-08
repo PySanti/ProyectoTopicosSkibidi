1345a96ea0f7042f47685938614fb542a7b558e0
AlejandroFlow2000 <alejanch2004@gmail.com>
2025-01-08 15:36:25 -0400

    Agregue la funcionalidad para buscar por ID

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { req_1 } from "./controllers/req_1.js";
 import { saveJoke } from "./controllers/req_2.js";
 import{validarObjectId, actualizarJoke } from "./controllers/req_3.js"
 import { eliminarJoke } from "./controllers/req_4.js";
+import { getJokeById } from "./controllers/req_5.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,6 +16,7 @@ app.get("/joke", req_1);
 app.post("/joke", saveJoke);
 app.put("/joke/:id", validarObjectId,actualizarJoke);
 app.delete("/joke/:id", eliminarJoke);
+app.get("/joke/:id", getJokeById);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
