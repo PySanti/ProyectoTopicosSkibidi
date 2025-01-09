002d11064af0ab0d3abc2b0ab1979c62ce460cea
AlejandroFlow2000 <alejanch2004@gmail.com>
2025-01-08 23:28:04 -0400

    Agregue la funcionalidad delete

---
+++ controllers/req_4.js
@@ -0,0 +1,18 @@
+import {Joke} from '../models/joke.js'; 
+
+
+export const req_4 = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const joke = await Joke.findByIdAndDelete(id);
+        if (!joke) {
+            console.log('Chiste no encontrado');
+            return res.status(404).json({ error: 'Chiste no encontrado' });
+        }
+        console.log('Chiste eliminado:', joke);
+        return res.status(200).json({ message: 'Chiste eliminado correctamente' });
+    } catch (error) {
+        console.error('Error al eliminar el chiste:', error);
+        return res.status(500).json({ error: 'Error al eliminar el chiste' });
+    }
+};
